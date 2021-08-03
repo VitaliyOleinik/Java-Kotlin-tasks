2157574e79644ec9f59a2a4d77e6d24a06bfee4f
VitaliyOleinik <oleynik624@gmail.com>
2021-08-03 22:03:30 +0600

    static initializer block

---
+++ Contest1/src/com/HackerRank.java
@@ -0,0 +1,31 @@
+package com;
+
+import java.util.Scanner;
+
+public class HackerRank {
+    static Scanner in = new Scanner(System.in);
+    static int B = in.nextInt();
+    static int H = in.nextInt();
+    static boolean flag = true;
+
+    static {
+        try {
+            if (B <= 0 || H <= 0) {
+                flag = false;
+                throw new Exception("Breadth and height must be positive");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+
+    public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+
+    }//end of main
+
+}//end of class

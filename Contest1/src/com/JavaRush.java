2d06fd43723ebb7bef666075dfd17602ec6fc471
VitaliyOleinik <oleynik624@gmail.com>
2021-08-03 21:50:24 +0600

    primitive type

--- Contest1/src/com/JavaRush.java
+++ Contest1/src/com/JavaRush.java
@@ -17,7 +17,7 @@ public class JavaRush {
         // \u000d System.out.println("выполняемый комментарий");
     }
 
-    public static void main(String[] args) {
+    void serialNumbers() {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
 
@@ -33,5 +33,31 @@ public class JavaRush {
             System.out.println("");
         }
         in.close();
+    }
+
+    void primitiveType() {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                if(x>=-32768 && x<=32767) System.out.println("* short");
+                if(x>=-Math.pow(2, 31) && x<=Math.pow(2, 31)-1) System.out.println("* int");
+                if(x>=-Math.pow(2, 63) && x<=Math.pow(2, 63)-1) System.out.println("* long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
